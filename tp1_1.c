21aabdf1424d7c245be94c8c70ed7b4aa8f08554
“Ezequiel <ezequielmartinez2424@gmail.com>
2023-03-22 22:42:34 -0300

    ejercicio 3.e hecho

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int *pnum , num = 15;
+    pnum = &num;
+    printf("Contenido del puntero: %d\n", *pnum);
+    printf("Direccion de memoria almacendada por el puntero: %d\n", pnum);
+    printf("Direccion de memoria de la variable: %d\n", &num);
+    printf("Direccion de memoria del puntero: %d\n", &pnum);
+    printf("Tamanio de memoria de la variable: %d Bytes\n", sizeof(*pnum));
+
+    
     return 0;
 }
